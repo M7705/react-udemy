82320b2977f60d8bc5e2a6abd5f233e3ab1215db
ES Choi <eschoi@ESui-MacBookAir.local>
2022-04-20 11:24:49 +0900

    add calculator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Heading from "./Heading";
+import {add, multiply, subtract, divide} from "./Calculator"
 
 function App() {
-    return (
+    return (<>
     <Heading />
+    <ul>
+        <li>{add(1, 2)}</li>
+        <li>{multiply(2, 3)}</li>
+        <li>{subtract(7, 2)}</li>
+        <li>{divide(5,2)}</li>
+    </ul>
+    </>
     );
 };
 
